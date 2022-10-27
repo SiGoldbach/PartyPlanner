0c360dd1c084083f8485a24c9e4d7123331ff9ab
Valdemar Nielsen <v.nielsen@live.dk>
2022-10-27 16:13:20 +0200

    Made login.kt

--- app/src/main/java/com/example/partyplanner/ui/theme/Login.kt
+++ app/src/main/java/com/example/partyplanner/ui/theme/Login.kt
@@ -1,19 +1,27 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.partyplanner.ui.theme.StandardButton
 import com.example.partyplanner.ui.theme.TopScreen
 import com.example.partyplanner.ui.theme.dustyRose
 
 @Composable
-fun MainScreen() {
-
+fun LoginScreen() {
     Column(
         modifier = Modifier
             .width(200.dp)
@@ -25,10 +33,12 @@ fun MainScreen() {
 
             ) {
             Column(verticalArrangement = Arrangement.Center, modifier = Modifier.padding(9.dp)) {
-                Text(text = "Velkommen",
+                Text(
+                    text = "Velkommen",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .wrapContentSize(Alignment.Center) )// .padding(start = 140.dp)
+                        .wrapContentSize(Alignment.Center)
+                )// .padding(start = 140.dp)
 
                 Box(
                     modifier = Modifier
@@ -36,22 +46,17 @@ fun MainScreen() {
                         .height(5.dp)
                         .background(dustyRose)
                         .clip(shape = RectangleShape)
-
                 )
             }
 
-
         }
-        Column {
-            StandardButton(output = "50'th birthday party")
-            StandardButton(output = "Uncles Marty's wedding")
-            StandardButton(output = "Button 3")
+        Column (verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp,200.dp)
+                ){
+            StandardButton(output = "Log ind")
+            StandardButton(output = "Opret bruger")
         }
-
-
     }
-
-
 }
-
-
