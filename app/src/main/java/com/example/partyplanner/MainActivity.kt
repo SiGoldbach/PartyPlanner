8414dee3e31a9fb023e655a3f04a6224ba0c3fdc
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2022-11-02 12:37:04 +0100

    Change in project to move Hylles stuff to another place

--- app/src/main/java/com/example/partyplanner/MainActivity.kt
+++ app/src/main/java/com/example/partyplanner/MainActivity.kt
@@ -3,20 +3,17 @@ package com.example.partyplanner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.example.partyplanner.ui.theme.*
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import kotlinx.coroutines.launch
+import com.example.partyplanner.ui.theme.MainScreen
+import com.example.partyplanner.ui.theme.NavigationDrawerComposeTheme
+import com.example.partyplanner.ui.theme.PartyPlannerTheme
+import com.example.partyplanner.ui.theme.TempMain
 
+/**
+ * When testing change the method called under the **ComposeTheme
+ */
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,83 +21,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
+
             NavigationDrawerComposeTheme {
-                val scaffoldState = rememberScaffoldState()
-                val scope = rememberCoroutineScope()
-                Scaffold(
-                    scaffoldState = scaffoldState,
-                    topBar = {
-                        AppBar(
-                            onNavigationIconClick = {
-                                scope.launch { scaffoldState.drawerState.open() }
-                            }
-                        )
-                    },
-                    //Gør så man ikke kan "dragge" hvis menuen er lukket
-                    drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
-                    drawerContent = {
-                        DrawerHeader()
-                        DrawerBody(
-                            items = listOf(
-                                MenuItem(
-                                    id = "menu",
-                                    title = "Menu",
-                                    contentDescription = "The menu",
-                                    icon = Icons.Filled.Menu
-                                ),
-                                MenuItem(
-                                    id = "profile",
-                                    title = "Profil",
-                                    contentDescription = "Go to profile",
-                                    icon = Icons.Filled.Face
-                                ),
-                                MenuItem(
-                                    id = "begivenhed",
-                                    title = "Begivenheder",
-                                    contentDescription = "Begivenheder",
-                                    icon = Icons.Filled.Add
-                                ),
-                                MenuItem(
-                                    id = "ønskeliste",
-                                    title = "Ønskeliste",
-                                    contentDescription = "Wishlist",
-                                    icon = Icons.Filled.Edit
-                                ),
-                                MenuItem(
-                                    id = "hjælp",
-                                    title = "Hjælp",
-                                    contentDescription = "support",
-                                    icon = Icons.Filled.Info
-                                ),
-                            ),
-                            onItemClick = {
-                                when(it.id){
-                                    "home" -> navController.navigate("home")
-                                    "event" -> navController.navigate("event")
-                                    "account" -> navController.navigate("account")
-                                }
-
-                                println("Clicked on ${it.title}")
-                            }
-                        )
-
-                    },
-                ){
-
-                }
+                //Change here
+                MainScreen(navController = navController)
             }
         }
     }
 }
 
 
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        PartyPlannerTheme {
-        }
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PartyPlannerTheme {
+        val navController = rememberNavController()
+        MainScreen(navController = navController)
     }
-
+}
 
 
 //PartyPlannerTheme {
---
+++ app/src/main/java/com/example/partyplanner/ui/theme/HylleScreen.kt
@@ -0,0 +1,82 @@
+package com.example.partyplanner.ui.theme
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavController
+import com.example.partyplanner.AppBar
+import com.example.partyplanner.DrawerBody
+import com.example.partyplanner.DrawerHeader
+import com.example.partyplanner.MenuItem
+import kotlinx.coroutines.launch
+
+@Composable
+fun TempMain(navController: NavController) {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            AppBar(
+                onNavigationIconClick = {
+                    scope.launch { scaffoldState.drawerState.open() }
+                }
+            )
+        },
+        //Gør så man ikke kan "dragge" hvis menuen er lukket
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+        drawerContent = {
+            DrawerHeader()
+            DrawerBody(
+                items = listOf(
+                    MenuItem(
+                        id = "menu",
+                        title = "Menu",
+                        contentDescription = "The menu",
+                        icon = Icons.Filled.Menu
+                    ),
+                    MenuItem(
+                        id = "profile",
+                        title = "Profil",
+                        contentDescription = "Go to profile",
+                        icon = Icons.Filled.Face
+                    ),
+                    MenuItem(
+                        id = "begivenhed",
+                        title = "Begivenheder",
+                        contentDescription = "Begivenheder",
+                        icon = Icons.Filled.Add
+                    ),
+                    MenuItem(
+                        id = "ønskeliste",
+                        title = "Ønskeliste",
+                        contentDescription = "Wishlist",
+                        icon = Icons.Filled.Edit
+                    ),
+                    MenuItem(
+                        id = "hjælp",
+                        title = "Hjælp",
+                        contentDescription = "support",
+                        icon = Icons.Filled.Info
+                    ),
+                ),
+                onItemClick = {
+                    when (it.id) {
+                        "home" -> navController.navigate("home")
+                        "event" -> navController.navigate("event")
+                        "account" -> navController.navigate("account")
+                    }
+
+                    println("Clicked on ${it.title}")
+                }
+            )
+
+        },
+    ) {
+
+    }
+}
+
