746d32688c0b3fd15d9e1d098275c91edf75b994
Valdemar Nielsen <v.nielsen@live.dk>
2022-11-07 11:34:56 +0100

    Login page is done (layout wise)

--- app/src/main/java/com/example/partyplanner/ui/theme/Login.kt
+++ app/src/main/java/com/example/partyplanner/ui/theme/Login.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -5,23 +6,56 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.partyplanner.ui.theme.*
+import com.example.partyplanner.R
 
-val standardDP: Dp = 10.dp
+
+@Composable
+fun LoginText(text: String) {
+    Text(
+        text = text,
+        fontStyle = FontStyle.Normal,
+        fontWeight = FontWeight.Bold,
+        fontSize = 38.sp, color = Color.Black
+    )
+
+}
+
+
+@Composable
+fun LoginScreenImage() {
+    val image = painterResource(id = R.drawable.champagnecelebration)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop,
+        )
+    }
+}
+@Preview
 @Composable
 fun LoginScreen() {
-    val a = Event("Login")
-    val b = Event("Opret Bruger")
-
-    val list = listOf(a, b)
-
+    LoginScreenImage()
     Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
         Box(
             contentAlignment = Alignment.Center,
@@ -32,71 +66,26 @@ fun LoginScreen() {
                     .padding(9.dp)
                     .fillMaxWidth()
             ) {
-                Button(
-
-                    onClick = { /*TODO*/ },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = dustyRose),
-                    shape = RoundedCornerShape(30.dp),
-                    modifier = Modifier
-                        .size(width = 250.dp, height = 50.dp)
-
-                ) {
-                    StandardText(text = "PartyPlanner")
+                Box() {
+                    LoginText(text = "Velkommen")
                 }
+
                 Spacer(modifier = Modifier.height(com.example.partyplanner.ui.theme.standardDP))
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(5.dp)
-                        .background(dustyRose)
+                        .height(2.dp)
+                        .background(Color.Black)
                         .clip(shape = RectangleShape)
 
                 )
             }
-
-
         }
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(list) { item ->
-                EventComposer(item)
-            }
-        }
-        /* Column{
-        Box(
-            contentAlignment = Alignment.Center,
 
-            ) {
-            Column(verticalArrangement = Arrangement.Center, modifier = Modifier.padding(9.dp)) {
-                Text(
-                    text = "Velkommen",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentSize(Alignment.Center)
-                )// .padding(start = 140.dp)
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth(1.0F)
-                        .height(5.dp)
-                        .background(dustyRose)
-                        .clip(shape = RectangleShape)
-                )
-            }
-
-        }
-        Column (verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                    // Changes location of buttons on screen
-            //    .fillMaxWidth()
-              //  .padding(0.dp,100.dp)
-                ){
-            StandardButton(output = "Log ind")
-            StandardButton(output = "Opret bruger")
-        }
-    }  */
+        Spacer(modifier = Modifier.height(425.dp))
+        StandardButton(output = "Login")
+        StandardButton(output = "Opret Bruger")
     }
+
 }
+
