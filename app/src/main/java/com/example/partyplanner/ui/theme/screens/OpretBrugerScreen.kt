692799ac1c4671cf3ba72ff3f1416853bc57691d
Valdemar Nielsen <v.nielsen@live.dk>
2022-11-08 11:01:28 +0100

    Updates to Opret bruger

--- app/src/main/java/com/example/partyplanner/ui/theme/screens/OpretBrugerScreen.kt
+++ app/src/main/java/com/example/partyplanner/ui/theme/screens/OpretBrugerScreen.kt
@@ -1,42 +1,79 @@
 package com.example.partyplanner.ui.theme.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.partyplanner.ui.theme.dustyRose
 
-
-// this shit dosent work
 @Composable
 fun OpretBruger(navController: NavController) {
-    var PersonalInformation by remember { mutableStateOf(TextFieldValue("")) }
+    var Fornavn by remember { mutableStateOf(TextFieldValue("")) }
+    var Efternavn by remember { mutableStateOf(TextFieldValue("")) }
+    var Email by remember { mutableStateOf(TextFieldValue("")) }
+    var Kodeord by remember { mutableStateOf(TextFieldValue("")) }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
     ) {
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Box() {
+            Text(
+                text = "Opret Bruger",
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Bold,
+                fontSize = 26.sp, color = Color.Black
+            )
+        }
         Spacer(modifier = Modifier.height(standardDP))
-        OutlinedTextField(colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = dustyRose),
-            value = PersonalInformation,
-            label = { Text(text = "Fornavn", color = dustyRose) },
-            onValueChange = { PersonalInformation = it }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(2.dp)
+                .background(Color.Black)
+                .clip(shape = RectangleShape)
         )
         Spacer(modifier = Modifier.height(standardDP))
         OutlinedTextField(colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = dustyRose),
-        value = PersonalInformation,
-        label = { Text(text= "Efternavn", color = dustyRose)},
-        onValueChange = {PersonalInformation = it }
+            value = Fornavn,
+            label = { Text(text = "Fornavn", color = dustyRose) },
+            onValueChange = { Fornavn = it }
+        )
+        Spacer(modifier = Modifier.height(standardDP))
+        OutlinedTextField(colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = dustyRose),
+            value = Efternavn,
+            label = { Text(text = "Efternavn", color = dustyRose) },
+            onValueChange = { Efternavn = it }
+        )
+        Spacer(modifier = Modifier.height(standardDP))
+        OutlinedTextField(colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = dustyRose),
+            value = Email,
+            label = { Text(text = "E-mail", color = dustyRose) },
+            onValueChange = { Email = it }
+        )
+        Spacer(modifier = Modifier.height(standardDP))
+        OutlinedTextField(colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = dustyRose),
+            value = Kodeord,
+            label = { Text(text = "Kodeord", color = dustyRose) },
+            onValueChange = { Kodeord = it }
         )
 
-        Spacer(modifier = Modifier.height(standardDP))
+        Spacer(modifier = Modifier.height(50.dp))
         StandardButton(output = "Opret")
 
     }
