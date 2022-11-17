38841f1ca4a82e9f70f123c2e2150063e7c27c13
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2022-11-17 17:41:46 +0100

    New profile composable and a lot off pictures

---
+++ app/src/main/java/com/example/partyplanner/ui/theme/screens/Profile.kt
@@ -0,0 +1,34 @@
+package com.example.partyplanner.ui.theme.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.partyplanner.R
+import com.example.partyplanner.ui.theme.dustyRose
+
+@Composable
+fun Profile(navController: NavController) {
+    Column(modifier = Modifier.fillMaxWidth().fillMaxHeight(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.height(10.dp))
+        Image(
+            painter = painterResource(id = R.drawable.profilepic),
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,            // crop the image if it's not a square
+            modifier = Modifier
+                .size(128.dp)
+                .clip(CircleShape)                       // clip to the circle shape
+                .border(4.dp, color = dustyRose, CircleShape)
+        )
+
+    }
+
+}
