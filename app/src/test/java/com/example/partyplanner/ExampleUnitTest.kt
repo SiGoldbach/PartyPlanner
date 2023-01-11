fc0f117ded250aaa4e4b1415e4a690e2d24f17b1
Christian H <christian@hyltoft.net>
2023-01-11 11:22:29 +0100

    mail smtp server, mailSender method created

--- app/src/test/java/com/example/partyplanner/ExampleUnitTest.kt
+++ app/src/test/java/com/example/partyplanner/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package com.example.partyplanner
 
+import com.example.partyplanner.fireBaseServices.MailSender
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.Properties
+import javax.mail.*
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +20,22 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+
+
+        @Test
+        fun sendEmailTest() {
+            val credentials = EmailCredentials("partyplannerdk@gmail.com", "isznshvcwlewebov")
+            val mailConfig = MailConfig("smtp.gmail.com", 587, true, true)
+            val mailProperties = MailProperties()
+
+            val props = mailProperties.getProperties(mailConfig)
+
+            Assertions.assertDoesNotThrow {
+                MailSender.sendEmail(credentials, props, "recipient@example.com", "Test Subject", "Test Body")
+
+        }
+    }
+
 }
+
