407ff49e5d58197c0d8c564b281f99e1cffb9263
Christian H <christian@hyltoft.net>
2023-01-16 13:34:07 +0100

    format

--- app/src/main/java/com/example/partyplanner/viewModel/ViewModelInvitations.kt
+++ app/src/main/java/com/example/partyplanner/viewModel/ViewModelInvitations.kt
@@ -1,20 +1,16 @@
 package com.example.partyplanner.viewModel
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.example.partyplanner.model.InvitationModel
 import com.example.partyplanner.model.Invitations
-import com.example.partyplanner.model.LoginUiState
-import com.example.partyplanner.ui.theme.screens.InvitationScreen
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class ViewModelInvitations : ViewModel() {
-    private var uiState = MutableStateFlow(InvitationModel( ))
+    private var uiState = MutableStateFlow(InvitationModel())
     val state: StateFlow<InvitationModel> = uiState.asStateFlow()
-
 
 
     fun addInviteToList(inviteItem: Invitations) {
@@ -33,7 +29,7 @@ class ViewModelInvitations : ViewModel() {
     fun markAsComplete(inviteItem: Invitations, value: Boolean) {
         var localList = uiState.value.listinvitation
         localList.find { it == inviteItem }?.isComplete = value
-        uiState.update { t->t.copy(listinvitation = localList) }
+        uiState.update { t -> t.copy(listinvitation = localList) }
 
 
     }
