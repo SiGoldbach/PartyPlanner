6553af44ec07438834845832a43ca26b5112111b
Valdemar Nielsen <v.nielsen@live.dk>
2023-01-17 12:55:58 +0100

    Now the tumbleweed and loadin icon is spinning. Very nice animation 10/10

--- app/src/main/java/com/example/partyplanner/ui/theme/screens/LoadingStateComposables.kt
+++ app/src/main/java/com/example/partyplanner/ui/theme/screens/LoadingStateComposables.kt
@@ -1,21 +1,22 @@
 package com.example.partyplanner.ui.theme.screens
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import com.example.partyplanner.R
-import com.example.partyplanner.naviagion.Destination
 import com.example.partyplanner.ui.theme.dustyRose
 
 /**
@@ -23,6 +24,13 @@ import com.example.partyplanner.ui.theme.dustyRose
  */
 @Composable
 fun loadingScreen(text: String) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val angle by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(tween(2000, easing = LinearEasing), RepeatMode.Restart)
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -35,6 +43,8 @@ fun loadingScreen(text: String) {
             painter = painterResource(id = R.drawable.loading_pic),
             contentDescription = stringResource(id = R.string.Coming_events),
             contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .rotate(angle)
         )
 
     }
@@ -43,6 +53,14 @@ fun loadingScreen(text: String) {
 
 @Composable
 fun emptyLoadingScreen(text: String, buttonText: String, lambda: () -> Unit) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val angle by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(tween(2000, easing = LinearEasing), RepeatMode.Restart)
+    )
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -51,11 +69,16 @@ fun emptyLoadingScreen(text: String, buttonText: String, lambda: () -> Unit) {
     ) {
         Text(text = text, fontSize = 16.sp)
         Spacer(modifier = Modifier.padding(10.dp))
-        Image(
-            painter = painterResource(id = R.drawable.tumble_weed_png_removebg_preview),
-            contentDescription = stringResource(id = R.string.Coming_events),
-            contentScale = ContentScale.Crop,
-        )
+
+            Image(
+                painter = painterResource(id = R.drawable.tumble_weed_png_removebg_preview),
+                contentDescription = stringResource(id = R.string.Coming_events),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .rotate(angle)
+            )
+Spacer(modifier = Modifier.height(20.dp))
+
         FloatingActionButton(
             onClick = { lambda.invoke() },
             backgroundColor = dustyRose,
@@ -66,4 +89,5 @@ fun emptyLoadingScreen(text: String, buttonText: String, lambda: () -> Unit) {
 
         }
     }
+
 }
